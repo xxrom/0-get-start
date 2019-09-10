7b8a9d8f7e35006236e35e88cb1e63ccdf9a88c4
nikita <xpom5566@mail.ru>
2019-09-11 00:48:53 +0300

    + simple connected Apollo example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import { ApolloProvider } from "@apollo/react-hooks";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Pages } from "./pages";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
-import gql from "graphql-tag";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -12,20 +15,30 @@ const client = new ApolloClient({
   cache,
   link
 });
-client
-  .query({
-    query: gql`
-      query GetLaunch {
-        launch(id: 56) {
-          id
-          mission {
-            name
-          }
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
+
+// run query !!!
+// import gql from "graphql-tag";
+// client
+//   .query({
+//     query: gql`
+//       query GetLaunch {
+//         launch(id: 56) {
+//           id
+//           mission {
+//             name
+//           }
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log("res", result));
 
 // import React from 'react';
 // import ReactDOM from 'react-dom';
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Pages = () => <h1>Pages</h1>;
+
+export { Pages };
